c273d28a54d1827b7eac95e9ab52a8855f4e7535
ariana124 <ariana.bibiano@gmail.com>
2020-08-20 23:21:13 -0700

    removed emoticon faces

--- app.js
+++ app.js
@@ -17,25 +17,25 @@ app.get("/", function(req, res) {
     
     switch (currentDay) {
         case 0 :
-            day = "Sunday :)";
+            day = "Sunday";
             break;
         case 1 :
-            day = "Monday :/";
+            day = "Monday";
             break;
         case 2 :
-            day = "Tuesday :|";
+            day = "Tuesday";
             break;
         case 3 :
-            day = "Wednesday :|";
+            day = "Wednesday";
             break;
         case 4 :
-            day = "Thursday :|";
+            day = "Thursday";
             break;
         case 5 :
-            day = "Friday :)";
+            day = "Friday";
             break;
         case 6 :
-            day = "Saturday :)";
+            day = "Saturday";
             break;
         default:
             console.log(`Error: current day is ${currentDay}`);
