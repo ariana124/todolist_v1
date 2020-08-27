549593fde26e8a41611d470cd195a95ef6f80878
ariana124 <ariana.bibiano@gmail.com>
2020-08-27 12:42:50 -0700

    removed switch cases and created new Date object to display the current date

--- app.js
+++ app.js
@@ -12,34 +12,14 @@ app.listen(3000, function() {
 app.get("/", function(req, res) {
 
     let today = new Date();
-    let currentDay = today.getDay();
-    let day = "";
-    
-    switch (currentDay) {
-        case 0 :
-            day = "Sunday";
-            break;
-        case 1 :
-            day = "Monday";
-            break;
-        case 2 :
-            day = "Tuesday";
-            break;
-        case 3 :
-            day = "Wednesday";
-            break;
-        case 4 :
-            day = "Thursday";
-            break;
-        case 5 :
-            day = "Friday";
-            break;
-        case 6 :
-            day = "Saturday";
-            break;
-        default:
-            console.log(`Error: current day is ${currentDay}`);
-    }
+
+    let options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    };
+
+    let day = today.toLocaleDateString("en-US", options);
 
     // Here we render an ejs file called list that exists within the views folder and we pass in the value of the variable kindOfDay with the type of day: weekend or weekday.
     res.render("list", {kindOfDay: day});
