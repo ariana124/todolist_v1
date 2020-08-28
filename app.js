6e4e25b2f1dbff1759abdecfbba6418ac30f7ca3
ariana124 <ariana.bibiano@gmail.com>
2020-08-28 15:08:33 -0700

    added public folder for express to use

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({extended: true}));
 
+app.use(express.static("public"));
+
 app.listen(3000, function() {
     console.log("Server is running on port 3000.");
 });
